9e7a2d25f6d7de6f8340155553aba03d8e5e1a55
brad191 <bradp191@gmail.com>
2022-05-23 21:27:02 -0700

    Add files via upload

---
+++ compat/rsi.hpp
@@ -0,0 +1,204 @@
+class RSI_86_BTFO_blk;
+class BTFO_blk;
+class BTFO_FDE;
+class BTFO_OD;
+class BTFO_blk_gl;
+class BTFO_FDE_gl;
+class BTFO_OD_gl;
+class Sig_556_65Creed_blk;
+class Sig_556_65Creed_tan;
+class Sig_556_65Creed_OD;
+class Sig_556_65Creed_multicam;
+class Sig_556_65Creed_woodland;
+class RSI_86_BTFO_blk_bcsewpn: RSI_86_BTFO_blk // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class BTFO_blk_bcsewpn: BTFO_blk // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class BTFO_FDE_bcsewpn: BTFO_FDE // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class BTFO_OD_bcsewpn: BTFO_OD // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class BTFO_blk_gl_bcsewpn: BTFO_blk_gl // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class BTFO_FDE_gl_bcsewpn: BTFO_FDE_gl // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class BTFO_OD_gl_bcsewpn: BTFO_OD_gl // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class Sig_556_65Creed_blk_bcsewpn: Sig_556_65Creed_blk // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class Sig_556_65Creed_tan_bcsewpn: Sig_556_65Creed_tan // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class Sig_556_65Creed_OD_bcsewpn: Sig_556_65Creed_OD // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class Sig_556_65Creed_multicam_bcsewpn: Sig_556_65Creed_multicam // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
+class Sig_556_65Creed_woodland_bcsewpn: Sig_556_65Creed_woodland // create a secondweapon classname
+{
+enableAttack = 0; // disable Attack
+type = 4; // launchsolt
+scope = 1; //hide in Arsenal
+scopeArsenal = 0; //hide in Arsenal
+handAnim[] = {}; // no handamin
+class Noob_Auto:Noob_Auto{};
+modes[] = {"Noob_Auto"}; //disable firemode
+class NGL: NGL{};
+muzzles[] = {"this","NGL"}; //disable UGL firemode _generalMacro = "";
+baseWeapon = "";
+hiddenSelections[] = {"zasleh"}; //maybe not zasleh but muzzleflash sometimes
+hiddenSelectionsTextures[] = {""}; // hide muzzleflash
+//hiddenSelectionsMaterials[] = {""}; //need it sometimes
+};
